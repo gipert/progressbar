4480f76199a2b316412dc2e619d067f802737c3b
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2016-12-10 00:08:35 +0100

    added demo program

---
+++ test.cc
@@ -0,0 +1,31 @@
+/* test program for the ProgressBar class
+ * 
+ * Author: Luigi Pertoldi
+ * Created: 9 dic 2016
+ *
+ * Compile: c++ -I. -o test test.cc progressbar.cc
+ * Usage: ./test
+ *
+ */
+
+#include <thread>
+#include <chrono>
+#include "progressbar.h"
+
+int main() {
+
+    int N = 36475;
+
+    ProgressBar bar(N);
+    bar.Init();
+
+    for ( int i = 0; i < N; i++ ) {
+
+        bar.Update(i);
+
+        // the program...
+        std::this_thread::sleep_for( std::chrono::microseconds(300) );
+    }
+
+    return 0;
+}
